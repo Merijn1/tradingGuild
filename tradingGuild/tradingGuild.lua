9d88a20fd890d274b810e9fce0c75a485070a4e8
merijn <merijnspiro@hotmail.com>
2026-05-19 19:38:57 +0200

    Hello world.

--- tradingGuild/tradingGuild.lua
+++ tradingGuild/tradingGuild.lua
@@ -1,6 +1,1 @@
-local frame = CreateFrame("Frame")
-
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", function()
-    SendChatMessage("Hello from my addon!", "SAY")
-end)
+message('My first addon!')
