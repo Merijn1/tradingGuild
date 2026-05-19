bc1faf3f6690275d9c5c7914a60ae1c0645b95f4
merijn <merijnspiro@hotmail.com>
2026-05-19 20:08:20 +0200

    Hello world.

--- tradingGuild/main.lua
+++ tradingGuild/main.lua
@@ -5,10 +5,9 @@ f:RegisterEvent("PLAYER_REGEN_ENABLED")
 
 f:SetScript("OnEvent", function(self,event, ...)
     if event == "PLAYER_REGEN_DISABLED" then
-        print("Hello world")
-    end
-
-    if event == "PLAYER_REGEN_ENABLED" then
-        print("You have left combat")
+        for i = 1, GetNumTradeSkills() do
+            --name, _ , _, _, _, _ = GetTradeSkillInfo(i)
+            print(GetTradeSkillInfo(i))
+        end
     end
 end)
