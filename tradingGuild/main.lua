86f2a053f558c9400eb5f79c3a0b9f599eba021b
merijn <merijnspiro@hotmail.com>
2026-05-19 19:48:51 +0200

    Hello world.

--- tradingGuild/main.lua
+++ tradingGuild/main.lua
@@ -1,1 +1,14 @@
-message('My first addon!')
+local f = CreateFrame("Frame")
+
+f:RegisterEvent("PLAYER_REGEN_DISABLED")
+f:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+f:SetScript("OnEvent", function(self,event, ...)
+    if event == "PLAYER_REGEN_DISABLED" then
+        print("Hello world")
+    end
+
+    if event == "PLAYER_REGEN_ENABLED" then
+        print("You have left combat")
+    end
+end)
